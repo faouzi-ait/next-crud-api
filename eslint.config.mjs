68fe9fcb20c83a64e7dc1d834892ae556cb59718
Admin <admin@Admins-MacBook-Air.local>
2025-01-05 12:17:40 +0000

    Linting Fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["src/app/api/blogs/[id]/route.ts"],
+    rules: {
+      "no-undef": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
